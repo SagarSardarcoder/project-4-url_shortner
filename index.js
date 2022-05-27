5139d452b0d404ef8cb2037893066b5769eccf59
SagarSardarcoder <102857635+SagarSardarcoder@users.noreply.github.com>
2022-05-27 12:32:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ mongoose.connect("mongodb+srv://uranium:uranium@cluster0.pgmlm.mongodb.net/proje
 
 
 app.use('/', route);
-
+app.use((req, res, next) => {
+    res.status(404).send({
+        status: 404,
+        error: `Not found ${req.url}`
+        
+    })
+    next()
+})
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
